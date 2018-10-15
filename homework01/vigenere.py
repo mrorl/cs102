f4fe528213d3a2525ad34620eb05c438e28a0aa2
mrorl <43522123+mrorl@users.noreply.github.com>
2018-10-15 17:34:11 +0300

    Реализована функция encrypt_vigenere(),decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,29 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    n = len(keyword)
+    m = 0
+    ciphertext = ''
+    if len(keyword) < len(plaintext):
+        a = len(plaintext) - len(keyword)
+        for i in range(len(keyword), len(plaintext)):
+            keyword += keyword[i - n]
+    while m < len(plaintext):
+        a = ord(plaintext[m])
+        b = ord(keyword[m])
+        if 97 <= a <= 122:
+            k = a + (b - 97)
+            if k > 122:
+                t = k - 123
+                k = 97 + t
+
+        elif 65 <= a <= 90:
+            k = a + (b - 65)
+            if k > 90:
+                t = k - 91
+                k = 65 + t
+        m += 1
+        ciphertext += chr(k)
     return ciphertext
 
 
@@ -24,5 +46,27 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    n = len(keyword)
+    m = 0
+    plaintext = ''
+    if len(keyword) < len(ciphertext):
+        a = len(ciphertext) - len(keyword)
+        for i in range(len(keyword), len(ciphertext)):
+            keyword += keyword[i - n]
+    while m < len(ciphertext):
+        a = ord(ciphertext[m])
+        b = ord(keyword[m])
+        if 97 <= a <= 122:
+            k = a - (b - 97)
+            if k < 97:
+                t = 96 - k
+                k = 122 - t
+
+        elif 65 <= a <= 90:
+            k = a - (b - 65)
+            if k < 65:
+                t = 64 - k
+                k = 90 - t
+        m += 1
+        plaintext += chr(k)
     return plaintext
