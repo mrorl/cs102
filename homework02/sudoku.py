bf3d7d0abe836c1a71fb9e99a8702372cc094f08
mrorl <43522123+mrorl@users.noreply.github.com>
2018-10-29 14:16:19 +0300

    Update sudoku

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,11 +1,14 @@
-def read_sudoku(filename):
+import random
+
+
+def read_sudoku(filename: str) -> list:
     """ Прочитать Судоку из указанного файла """
     digits = [c for c in open(filename).read() if c in '123456789.']
     grid = group(digits, 9)
     return grid
 
 
-def display(values):
+def display(values: list) -> None:
     """Вывод Судоку """
     width = 2
     line = '+'.join(['-' * (width * 3)] * 3)
@@ -14,9 +17,10 @@ def display(values):
         if str(row) in '25':
             print(line)
     print()
+    return None
 
 
-def group(values, n):
+def group(values: list, n: int) -> list:
     """
     Сгруппировать значения values в список, состоящий из списков по n элементов
 
@@ -25,10 +29,16 @@ def group(values, n):
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    data = [[0 for i in range(n)] for j in range(n)]
+    a = 0
+    for i in range(n):
+        for j in range(n):
+            data[i][j] = values[a]
+            a += 1
+    return data
 
 
-def get_row(values, pos):
+def get_row(values: list, pos: tuple) -> list:
     """ Возвращает все значения для номера строки, указанной в pos
 
     >>> get_row([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
@@ -38,10 +48,11 @@ def get_row(values, pos):
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    pass
+    row, _ = pos
+    return values[row]
 
 
-def get_col(values, pos):
+def get_col(values: list, pos: tuple) -> list:
     """ Возвращает все значения для номера столбца, указанного в pos
 
     >>> get_col([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
@@ -51,10 +62,15 @@ def get_col(values, pos):
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    pass
+    _, col = pos
+    collist = []
+    for i in range(len(values)):
+        for j in range(0, len(values[i]), 3):
+            collist.append(values[i][col])
+    return collist
 
 
-def get_block(values, pos):
+def get_block(values: list, pos: tuple) -> list:
     """ Возвращает все значения из квадрата, в который попадает позиция pos
 
     >>> grid = read_sudoku('puzzle1.txt')
@@ -65,10 +81,18 @@ def get_block(values, pos):
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+    row, col = pos
+    block = []
+    a = row // 3
+    b = col // 3
+    for i in range(len(values)):
+        for j in range(len(values[i])):
+            if (i // 3 == a and j // 3 == b):
+                block.append(values[i][j])
+    return block
 
 
-def find_empty_positions(grid):
+def find_empty_positions(grid: list) -> any:
     """ Найти первую свободную позицию в пазле
 
     >>> find_empty_positions([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']])
@@ -78,10 +102,14 @@ def find_empty_positions(grid):
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if (grid[i][j] is '.'):
+                return(i, j)
+    return None
 
 
-def find_possible_values(grid, pos):
+def find_possible_values(grid: list, pos: tuple) -> set:
     """ Вернуть множество возможных значения для указанной позиции
 
     >>> grid = read_sudoku('puzzle1.txt')
@@ -92,10 +120,16 @@ def find_possible_values(grid, pos):
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
+    num = set('123456789')
+    a = set(get_row(grid, pos))
+    b = set(get_col(grid, pos))
+    c = set(get_block(grid, pos))
+    s = a.union(b, c)
+    num = num.difference(s)
+    return num
 
 
-def solve(grid):
+def solve(grid: list) -> any:
     """ Решение пазла, заданного в grid """
     """ Как решать Судоку?
         1. Найти свободную позицию
@@ -108,16 +142,39 @@ def solve(grid):
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    pos: tuple = find_empty_positions(grid)
+    if not pos:
+        return grid
+    row, col = pos
+    for i in find_possible_values(grid, pos):
+        grid[row][col] = i
+        solution: list = solve(grid)
+        if solution:
+            return solution
+    grid[row][col] = '.'
+    return None
 
 
-def check_solution(solution):
+def check_solution(solution: list) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
     # TODO: Add doctests with bad puzzles
-    pass
+    for row in range(len(solution)):
+        a = set(get_row(solution, (row, 0)))
+        if a != set('123456789'):
+            return False
+    for col in range(len(solution)):
+        b = set(get_row(solution, (0, col)))
+        if b != set('123456789'):
+            return False
+    for i in range(0, len(solution), 3):
+        for j in range(0, len(solution[i]), 3):
+            c = set(get_row(solution, (i, j)))
+            if b != set('123456789'):
+                return False
+    return True
 
 
-def generate_sudoku(N):
+def generate_sudoku(N: int) -> list:
     """ Генерация судоку заполненного на N элементов
 
     >>> grid = generate_sudoku(40)
@@ -139,7 +196,15 @@ def generate_sudoku(N):
     >>> check_solution(solution)
     True
     """
-    pass
+    grid: list = solve([['.'] * 9 for _ in range(9)])
+    N = 81 - min(81, max(0, N))
+    while N:
+        row = random.randint(0, 8)
+        col = random.randint(0, 8)
+        if grid[row][col] != '.':
+            grid[row][col] = '.'
+            N -= 1
+    return grid
 
 
 if __name__ == '__main__':
